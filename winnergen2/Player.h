4173ebf20d9646ce1d3e032ddd3973059a62a7fd
Nick Schultz <nick.schultz@live.com>
2011-12-05 21:22:02 -0800

    Added Gamestate, might work?

--- winnergen2/Player.h
+++ winnergen2/Player.h
@@ -2,35 +2,42 @@
 #include <string>
 #include <vector>
 
+class Contest;
+typedef enum{TeamA,	TeamB, NOTSET} GamePick;
 
+class PlayerPick
+{
+	PlayerPick(Contest &game, GamePick pick, int rank):
+		m_Game(game), m_Rank(rank), m_Pick(pick){}
+
+	const Contest& m_Game;
+	const GamePick m_Pick;
+	const int m_Rank;
+};
+typedef std::vector<PlayerPick> PickList;
 
 class Player
 {
 public:
-	Player(void);
-	~Player(void);
+	Player(std::string name):
+		m_PlayerName(name){}	
 
-	int m_GameRankings[35];
-	
-	enum{
-		A,
-		B
-	} m_GameWinners[35];
-
-	std::string m_PlayerName;
+	PickList picks;
+	const std::string m_PlayerName;
 };
+typedef std::vector<Player> PlayerList;
 
 class Contest{
-	enum{
-		TeamA,
-		TeamB,
-		NOTSET
-	} m_GameWinner;
-};
+	Contest(std::string &teamA, std::string &teamB, GamePick initPick):
+		m_TeamA(teamA),m_TeamB(teamB),m_InitOutcome(initPick){}
 
-typedef std::vector<Player> PlayerList;
+	const GamePick m_InitOutcome;  
+	const std::string m_TeamA;
+	const std::string m_TeamB;
+};
 typedef std::vector<Contest> ContestList;
 
+//Should have only one instance of this class, threads will refer to this.
 class GameState
 {
 	PlayerList m_PlayerList;
